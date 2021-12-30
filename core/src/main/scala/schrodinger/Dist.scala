f14b14411d19c16a560c3b97c7b23f81073ce416
Arman Bilge <armanbilge@gmail.com>
2021-12-30 01:12:07 +0000

    Add Dist monad

---
+++ core/src/main/scala/schrodinger/Dist.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package schrodinger
+
+import algebra.ring.AdditiveMonoid
+import algebra.ring.CommutativeSemifield
+import algebra.ring.MultiplicativeMonoid
+import algebra.ring.Semifield
+import cats.CommutativeMonad
+import cats.Monad
+import cats.instances.*
+import cats.kernel.Semigroup
+import cats.kernel.instances.MapMonoid
+import cats.syntax.all.*
+
+final case class Dist[P, A](support: Map[A, P]):
+
+  def map[B](f: A => B)(using P: AdditiveMonoid[P]): Dist[P, B] =
+    Dist(support.groupMapReduce(kv => f(kv._1))(_._2)(P.plus(_, _)))
+
+  def flatMap[B](f: A => Dist[P, B])(using P: Semifield[P]): Dist[P, B] =
+    Dist {
+      MapMonoid[B, P](using P.additive).combineAll {
+        support.view.map((a, p) => f(a).support.view.mapValues(P.times(p, _)).toMap)
+      }
+    }
+
+object Dist:
+  def pure[P, A](a: A)(using P: MultiplicativeMonoid[P]): Dist[P, A] =
+    Dist(Map(a -> P.one))
+
+  def monad[P: Semifield](n: Int): Monad[Dist[P, *]] = DistMonad(n)
+
+  def commutativeMonad[P: CommutativeSemifield](n: Int): Monad[Dist[P, *]] =
+    new DistMonad(n) with CommutativeMonad[Dist[P, *]]
+
+  private class DistMonad[P](n: Int)(using P: Semifield[P]) extends Monad[Dist[P, *]]:
+    private given Semigroup[P] = P.additive
+
+    def pure[A](a: A): Dist[P, A] = Dist.pure(a)
+
+    override def map[A, B](da: Dist[P, A])(f: A => B): Dist[P, B] = da.map(f)
+
+    def flatMap[A, B](da: Dist[P, A])(f: A => Dist[P, B]): Dist[P, B] = da.flatMap(f)
+
+    def tailRecM[A, B](a: A)(f: A => Dist[P, Either[A, B]]): Dist[P, B] =
+      var fa = f(a)
+      var db = Map.empty[B, P]
+      (0 until n).foreach { _ =>
+        val as = fa.support.collect { case (Left(a), p) => a -> p }
+        fa = Dist(as).flatMap(f)
+        val bs = fa.support.collect { case (Right(b), p) => b -> p }
+        db = db |+| bs
+      }
+      Dist(db)
