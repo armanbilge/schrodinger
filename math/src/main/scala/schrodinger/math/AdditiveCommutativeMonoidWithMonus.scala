070321c862fa88ce6b65c60445ae8e1221505567
Arman Bilge <armanbilge@gmail.com>
2022-04-21 00:39:29 +0000

    Add a reference

--- math/src/main/scala/schrodinger/math/AdditiveCommutativeMonoidWithMonus.scala
+++ math/src/main/scala/schrodinger/math/AdditiveCommutativeMonoidWithMonus.scala
@@ -19,6 +19,10 @@ package schrodinger.math
 import algebra.ring.AdditiveCommutativeMonoid
 import cats.kernel.PartialOrder
 
+/**
+ * Amer, K. Equationally complete classes of commutative monoids with monus. Algebra Universalis
+ * 18, 129–131 (1984). [[https://doi.org/10.1007/BF01182254]]
+ */
 trait AdditiveCommutativeMonoidWithMonus[A] extends AdditiveCommutativeMonoid[A]:
   def monus(x: A, y: A): A
 
