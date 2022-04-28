605b590bef482ab849013733adc96127533c723a
Arman Bilge <armanbilge@gmail.com>
2022-04-28 22:52:30 +0000

    Implement Rig.fromInt

--- math/src/main/scala/schrodinger/math/syntax.scala
+++ math/src/main/scala/schrodinger/math/syntax.scala
@@ -19,8 +19,21 @@ package schrodinger.math.syntax
 import algebra.ring.AdditiveSemigroup
 import algebra.ring.MultiplicativeGroup
 import algebra.ring.MultiplicativeSemigroup
+import algebra.ring.Rig
+import algebra.ring.Ring
 
 extension [A](x: A)
   def +(y: A)(using A: AdditiveSemigroup[A]): A = A.plus(x, y)
   def *(y: A)(using A: MultiplicativeSemigroup[A]): A = A.times(x, y)
   def /(y: A)(using A: MultiplicativeGroup[A]): A = A.div(x, y)
+
+extension (rig: Rig.type)
+  def fromInt[A: Rig](n: Int): A = fakeRing.fromInt(n)
+  def fromBigInt[A: Rig](n: BigInt): A = fakeRing.fromBigInt(n)
+
+  private def fakeRing[A](using A: Rig[A]): Ring[A] = new:
+    override def zero = A.zero
+    override def one = A.one
+    override def plus(x: A, y: A) = A.plus(x, y)
+    override def times(x: A, y: A) = A.times(x, y)
+    override def negate(x: A) = throw new UnsupportedOperationException
