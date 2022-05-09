0279b477f1743164730b281d7f6d23d69f38ac79
Arman Bilge <armanbilge@gmail.com>
2022-05-09 03:38:55 +0000

    Add DensitySpec

---
+++ stats/src/test/scala/schrodinger/stats/DensitySpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package schrodinger.stats
+
+import org.specs2.ScalaCheck
+import org.specs2.mutable.Specification
+import org.typelevel.discipline.specs2.mutable.Discipline
+import cats.laws.discipline.InvariantMonoidalTests
+import algebra.instances.long.*
+import cats.Eval
+import org.scalacheck.Arbitrary
+import cats.Functor
+import cats.syntax.all.*
+import cats.laws.discipline.arbitrary.*
+import cats.kernel.Eq
+import cats.laws.discipline.eq.*
+import cats.laws.discipline.MiniInt
+
+class DensitySpec extends Specification, Discipline, ScalaCheck:
+
+  given [F[_]: Functor, A](using arb: Arbitrary[A => F[Int]]): Arbitrary[Density[F, Long, A]] =
+    Arbitrary(arb.arbitrary.map(_.andThen(_.map(_.toLong.abs))).map(Density(_)))
+
+  given [F[_], P, A](using eq: Eq[A => F[P]]): Eq[Density[F, P, A]] =
+    eq.imap(Density(_))(x => x)
+
+  checkAll(
+    "Density",
+    InvariantMonoidalTests[Density[Eval, Long, _]].invariantMonoidal[MiniInt, MiniInt, MiniInt]
+  )
