8f62207448a002dd8035ed1c12e7927a8e94f6dc
Arman Bilge <armanbilge@gmail.com>
2022-11-17 21:08:03 +0000

    Add some `inline`s

--- math/native/src/main/scala/schrodinger/math/special/gamma.scala
+++ math/native/src/main/scala/schrodinger/math/special/gamma.scala
@@ -19,8 +19,8 @@ package special
 
 import scala.scalanative.libc.math.*
 
-def logGamma(x: Double): Double = lgamma(x)
-def gamma(x: Double): LogDouble = LogDouble.exp(logGamma(x))
+inline def logGamma(x: Double): Double = lgamma(x)
+inline def gamma(x: Double): LogDouble = LogDouble.exp(logGamma(x))
 
 def beta(p: Double, q: Double): LogDouble =
   (gamma(p) * gamma(q)) / gamma(p + q)
